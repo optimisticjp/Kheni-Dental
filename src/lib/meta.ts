72bc6bd2dce419b15260091d0c221205ee431321
optimisticjp <optimistic.jp@gmail.com>
2026-09-19 08:27:37 +0530

    Add Meta Pixel configuration

---
+++ src/lib/meta.ts
@@ -0,0 +1,7 @@
+export const META_PIXEL_ID = "1055188140670527";
+
+export const META_CONTACT_EVENTS: ReadonlySet<string> = new Set([
+  "whatsapp_click",
+  "phone_click",
+  "appointment_submit",
+]);
